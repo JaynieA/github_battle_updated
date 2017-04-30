50b23ec1267f95942df232a1999f818aaed82148
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-04-29 19:45:38 -0500

    add routing

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry:'./app/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -15,6 +16,9 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [new HtmlWebpackPlugin({
     template: 'app/index.html'
