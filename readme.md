2bb8d4f2ca72c2ca03ab1a98b894b947945b9a22
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-05-02 19:14:49 -0500

    deploy app

--- readme.md
+++ readme.md
@@ -2,7 +2,11 @@
 
 Building a React application while following the **recently updated** [React Fundamentals tutorial](https://reacttraining.com/online).
 
+Final application hosted [here](https://github-battle-6d478.firebaseapp.com).
+
 ## Installation
+
+This app requires the env vars: `GITHUB_API_ID` and `GITHUB_API_SECRET`.
 
 From the root directory, run:
 `npm install`, then
@@ -14,5 +18,6 @@ From the root directory, run:
   * Webpack
   * Babel
   * Axios
+  * Firebase
   * HTML5
   * CSS3
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,53 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+//NODE_ENV to production
+//uglify
+
+let config = {
   entry:'./app/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
     publicPath: '/'
   },
-  module: {
-    rules: [
-      {test: /\.(jsx)$/, use: 'babel-loader'},
-      {test: /\.css$/, use: ['style-loader', 'css-loader'] }
-    ]
-  },
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {test:  /\.jsx?$/, use: ['babel-loader']},
+      {test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
   },
   devServer: {
     historyApiFallback: true
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'app/index.html'
-  }),
-  new webpack.EnvironmentPlugin(['GITHUB_API_ID', 'GITHUB_API_SECRET'])]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
 };
+
+//If we are not building for production
+if(process.env.NODE_ENV !== 'production') {
+  config.plugins.push(
+    new webpack.EnvironmentPlugin(['GITHUB_API_ID', 'GITHUB_API_SECRET'])
+  );
+}
+
+//If we are building for production
+if(process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env' : {
+        'NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
+module.exports = config;
